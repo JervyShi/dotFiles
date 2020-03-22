3d9241d669d8f84ada2a6f7371592cc400dbd8c4
jervyshi <jervyshi@gmail.com>
2020-03-23 01:59:17 +0800

    add jdk change aliases

---
+++ java/alias.zsh
@@ -0,0 +1,5 @@
+export JAVA_7_HOME=$(/usr/libexec/java_home -v 1.7)
+export JAVA_8_HOME=$(/usr/libexec/java_home -v 1.8)
+
+alias jdk7="export JAVA_HOME=$JAVA_7_HOME"
+alias jdk8="export JAVA_HOME=$JAVA_8_HOME"
